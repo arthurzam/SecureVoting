48006b767b216a699849ec614e5cb9a7e3d99285
Arthur Zamarin <arthurzam@gentoo.org>
2024-11-20 17:45:53 +0200

    improve texts by Tamir

--- src/tallier/mail.py
+++ src/tallier/mail.py
@@ -24,18 +24,17 @@ def register_email(to: str, name: str, secret_number: int):
         s.send_message(build_msg(to, 'Register to aVote', content))
 
 def start_election(manager_name: str, election: mytypes.Election, voters: Iterable[str]):
-    content = f"A new election had opened.\n" \
+    content = f"A new election was initiated.\n" \
         f"Election \"{election.election_name}\", by {manager_name} <{election.manager_email}>.\n" \
-        "You can vote in it, after logging in, using the following link:\n" \
+        "You can cast your vote after logging in, using the following link:\n" \
         f"{WEBSITE_URL}/election/vote?id={election.election_id}\n"
     with SMTP('mailserver') as s:
         for voter in {election.manager_email} | set(voters):
             s.send_message(build_msg(voter, 'Election Started', content))
 
 def stop_election(manager_name: str, election: mytypes.Election, voters: Iterable[str], results: Iterable[str]):
-    content = f"An election you were participating in had closed.\n" \
-        f"Election \"{election.election_name}\", by {manager_name} <{election.manager_email}>.\n" \
-        "The results are the following:\n" \
+    content = f"Election \"{election.election_name}\" is now closed.\n" \
+        "The winners are:\n" \
         + "\n".join((f"    Place #{place} - {name}" for place, name in enumerate(results, start=1)))
     with SMTP('mailserver') as s:
         for voter in {election.manager_email} | set(voters):
