a4c4745a9335d08665782c0d6e06963b9679f9f7
Arthur Zamarin <arthurzam@gentoo.org>
2025-01-23 19:42:18 +0200

    fixup! tallier: allow re-register using existing email

--- src/tallier/db.py
+++ src/tallier/db.py
@@ -31,7 +31,7 @@ class DBconn:
                 await self.conn.execute("""
                     INSERT INTO users(email, name, secret_number)
                     VALUES ($1, $2, $3)
-                    ON CONFLICT DO UPDATE SET name = $2, secret_number = $3
+                    ON CONFLICT (email) DO UPDATE SET name = $2, secret_number = $3
                 """, email, name, secret_number)
                 return True
         except asyncpg.UniqueViolationError:
